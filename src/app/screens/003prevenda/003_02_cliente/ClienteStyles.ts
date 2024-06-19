310ee73b956ffe49a9c7048b9fe8dcb7a408005d
Rebeca Alvarez <rebeca@cs-consoft.com.br>
2024-06-19 18:26:23 -0300

    Desenvolvimento tela pre-venda cliente
    Continuação do desenvolvimento tela pre-venda cliente:

---
+++ src/app/screens/003prevenda/003_02_cliente/ClienteStyles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const ClienteStyles = StyleSheet.create({
+    btn_squad:{
+        backgroundColor: '#fff', 
+        borderRadius:10, 
+        elevation: 1, 
+        width: 40, 
+        height:40, 
+        alignSelf: 'flex-end', 
+        marginTop: 10,
+    }
+});
+
+
+
+
