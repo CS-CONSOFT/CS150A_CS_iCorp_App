7e703448717ae086bd5c65e92d4e04b8eeb1fdb4
Rebeca Alvarez <rebeca@cs-consoft.com.br>
2024-07-08 18:25:47 -0300

    async PDF
    async PDF

--- src/app/screens/005obras/CS_SC_005_04_Anexos.tsx
+++ src/app/screens/005obras/CS_SC_005_04_Anexos.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pressable, SafeAreaView, Text, StyleSheet, Dimensions, Alert } from "react-native";
+import { Pressable, SafeAreaView, Text, StyleSheet, Dimensions, Alert, ActivityIndicator } from "react-native";
 import CustomCard_002 from "../../components/cards/CustomCard_002";
 import { ICON_NAME } from "../../util/IconsName";
 import { useEffect, useState } from 'react';
@@ -12,28 +12,49 @@ import WebView from 'react-native-webview';
  */
 const CS_SC_005_04_Anexos = () => {
 
+    const api =  axios.create({
+        baseURL: "http://docente.ifsc.edu.br/"
+    })
+
 
     const [show, setShow] = useState<Boolean>(false);
+    const [arquivo, setArquivo] = useState<any>();
     const aquivoPdf: any | undefined = { uri: 'http://docente.ifsc.edu.br/vilson.junior/pi/04_Introducao_JavaScript.pdf' };
-
    
     const pressShow =  () => {
         setShow(!show)
         console.log("clicado")
     }
 
-    function ShowPdf(){
+    function ShowPdf(filePdf: any){
         return(
             <WebView
-                source={aquivoPdf} 
+                source={filePdf} 
                 style={{flex: 1}}
                 onNavigationStateChange={navState => 
                 navState.url !== aquivoPdf &&
                     setShow(false)
                 }
-            />         
+            />
+                    
         )
     }
+
+
+    
+    const downloadPDF = async () => {
+        const pdfUrl: string = 'vilson.junior/pi/04_Introducao_JavaScript.pdf';
+      
+        try {
+            const response = await api.get(`${pdfUrl}`);
+            setShow(true)
+            setArquivo(response)
+            alert('PDF baixado com sucesso!');
+        } catch (error) {
+            Alert.alert(`Erro ao baixar o PDF:${error}`);
+        }
+    };
+    
 
     return (
         <SafeAreaView style={styles.contanier}>
@@ -43,20 +64,19 @@ const CS_SC_005_04_Anexos = () => {
             <Pressable 
             onPress={ () => {
                 pressShow()
+                Alert.alert("PDF baixado com sucesso")
             }} 
             style={{width:"90%"}}
-            >
-                
+            >   
+                {
+                    show && (
+                        ShowPdf(aquivoPdf)
+                    )
+                }                
                 <CustomCard_002 
                 icon={ICON_NAME.DOCUMENT} 
                 title="PDF do projeto"
                 />
-                {
-                    show && (
-                        ShowPdf()
-                    )
-                }
-                    
                 
             </Pressable>
         </SafeAreaView>
