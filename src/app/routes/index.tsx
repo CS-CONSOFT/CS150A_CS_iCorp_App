0294dcb96354358cb399793a55786bfa711b497e
Valter <valter.gabriel@cs-consoft.com.br>
2024-08-08 17:57:02 -0300

    update

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -2,11 +2,13 @@ import { NavigationContainer } from '@react-navigation/native'
 import StackRoutes from './stack.routes'
 
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar';
 
 export default function Routes() {
     return (
         <SafeAreaProvider>
             <NavigationContainer>
+                <StatusBar style='auto' />
                 <StackRoutes />
             </NavigationContainer>
         </SafeAreaProvider>
