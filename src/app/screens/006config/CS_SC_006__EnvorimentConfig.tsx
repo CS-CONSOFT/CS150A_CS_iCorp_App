ddc2202043a0baa606f9fc83f4a06e6ef04e9229
Valter <valter.gabriel@cs-consoft.com.br>
2024-08-27 10:58:12 -0300

    Ajustando tela de configu

--- src/app/screens/006config/CS_SC_006__EnvorimentConfig.tsx
+++ src/app/screens/006config/CS_SC_006__EnvorimentConfig.tsx
@@ -1,6 +1,6 @@
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { useCallback, useState } from "react";
-import { Alert, ImageBackground, StyleSheet, Text, TextInput, TouchableHighlight, View } from "react-native";
+import { Alert, ImageBackground, StyleSheet, Text, TextInput, TouchableHighlight, TouchableWithoutFeedback, View } from "react-native";
 import { commonStyle } from "../../CommonStyle";
 import CustomIcon from "../../components/icon/CustomIcon";
 import CustomLoading from "../../components/loading/CustomLoading";
@@ -24,13 +24,20 @@ const CS_SC_006__EnvorimentConfig = ({ route }: { route: any }) => {
     const [nomeCotacao, setNomeCotacao] = useState('-');
     const [hasValue, setHasValue] = useState(false);
     const [isLoading, setIsLoading] = useState(false)
+    const [isEnviromentValidated, setIsEnviromentValidates] = useState(false)
     const { navigate } = useNavigation();
     const db = useDatabase();
 
     // useEffect para carregar os dados iniciais
     useFocusEffect(
         useCallback(() => {
-            get()
+            getSimpleData(DataKey.IsConfigValidada).then((res) => {
+                if (res == '1') {
+                    setIsEnviromentValidates(true)
+                }
+                get()
+            })
+
         }, [])
     );
 
@@ -46,24 +53,16 @@ const CS_SC_006__EnvorimentConfig = ({ route }: { route: any }) => {
             showToast(ToastType.ERROR, "Dados Insuficientes!", "Preencha os dados corretamente para avançar!")
             setIsLoading(false)
         } else {
-            validaAmbiente({ tenant: Number(_tenant), token: _token }).then((res) => {
-                if (res.Retorno.IsOk) {
-                    storeSimpleDataVc(DataKey.IsConfigValidada, "1").then(() => {
-                        navigate('Login');
-                        storeSimpleData(DataKey.MaintainOpenConfig, "false")
-                    })
-                }
-                setIsLoading(false)
-            }).catch((res) => {
-                showToast(ToastType.ERROR, "Erro", "Um erro ocorreu, verifique as informações")
-                setIsLoading(false)
-            })
+            setIsLoading(false)
+            navigate('Login');
+            storeSimpleData(DataKey.MaintainOpenConfig, "false")
         }
 
     }
 
     // Função assíncrona para buscar dados do banco de dados
     async function get() {
+        setIsLoading(true)
         try {
             await db.get().then((response) => {
                 if (response != null) {
@@ -85,6 +84,7 @@ const CS_SC_006__EnvorimentConfig = ({ route }: { route: any }) => {
                                 init(response.tenantId!, response.urlBase, response.token)
                             }
                         })
+                        setIsLoading(false)
 
                     });
 
@@ -113,7 +113,6 @@ const CS_SC_006__EnvorimentConfig = ({ route }: { route: any }) => {
                 storeSimpleData(DataKey.NomeCotacao, nomeCotacao).then(() => {
                     get().then(() => {
                         setIsLoading(false)
-                        init(tenant, urlBase, token)
                     }); // Buscar dados atualizados após a criação
                 })
             });
@@ -136,6 +135,26 @@ const CS_SC_006__EnvorimentConfig = ({ route }: { route: any }) => {
     if (isLoading) {
         return <CustomLoading />
     }
+    function validate(tenant: string, urlBase: string, token: string): void {
+        validaAmbiente({ tenant: Number(tenant), token: token }).then((res) => {
+            if (res.Retorno.IsOk) {
+                storeSimpleDataVc(DataKey.IsConfigValidada, "1").then(() => {
+                    storeSimpleData(DataKey.MaintainOpenConfig, "false")
+                    setIsEnviromentValidates(true)
+                })
+            } else {
+                storeSimpleDataVc(DataKey.IsConfigValidada, "0").then(() => {
+                    storeSimpleData(DataKey.MaintainOpenConfig, "true")
+                    setIsEnviromentValidates(false)
+                })
+            }
+            setIsLoading(false)
+        }).catch((res) => {
+            showToast(ToastType.ERROR, "Erro", "Um erro ocorreu, verifique as informações")
+            setIsLoading(false)
+        })
+    }
+
     // Renderização do componente
     return (
         <View style={{ flex: 1 }}>
@@ -162,87 +181,106 @@ const CS_SC_006__EnvorimentConfig = ({ route }: { route: any }) => {
                             </TouchableHighlight>
 
                             <TouchableHighlight
-                                onPress={() => init(tenant, urlBase, token)}
+                                onPress={() => validate(tenant, urlBase, token)}
                                 style={commonStyle.common_button_style}
                                 underlayColor='white'
                             >
                                 <Text style={commonStyle.common_text_button_style}>Validar</Text>
                             </TouchableHighlight>
                         </View>
-                    </View>
-                )}
 
-                {!hasValue && (
-                    // Se não houver valores, exibir campos de entrada para configuração
-                    <View style={[commonStyle.common_margin_top_64, commonStyle.common_margin_horizontal]}>
-                        <View style={[commonStyle.common_columnItem]}>
-
-                            <View style={[commonStyle.common_rowItem, commonStyle.justify_content_space_btw, commonStyle.common_margin_bottom_8]}>
-                                <Text style={[commonStyle.common_fontWeight_600, { color: '#fff', fontSize: 16 }]}>Tenant</Text>
-                                <CustomIcon icon={ICON_NAME.CAMERA} iconColor="#fff" iconSize={28} onPress={() => {
-
-                                    navigate('Camera', {
-                                        previousScreen: 'Config_Ambiente'
-                                    })
-
-                                }} />
-                            </View>
-                            <TextInput
-                                style={[commonStyle.common_input, commonStyle.common_margin_bottom_16]}
-                                onChangeText={setTenant}
-                                placeholder="Digite o tenant"
-                                keyboardType='decimal-pad'
-                                value={tenant}
-                            />
-
-                            <Text style={[commonStyle.common_margin_bottom_8, commonStyle.common_fontWeight_600, { color: '#fff', fontSize: 16 }]}>URL</Text>
-
-                            <TextInput
-                                style={[commonStyle.common_input, commonStyle.common_margin_bottom_16]}
-                                onChangeText={setUrlBase}
-                                placeholder="Digite a URL"
-                                value={urlBase}
-                            />
-
-
-                            <Text style={[commonStyle.common_margin_bottom_8, commonStyle.common_fontWeight_600, { color: '#fff', fontSize: 16 }]}>Token</Text>
-
-                            <TextInput
-                                style={[commonStyle.common_input, commonStyle.common_margin_bottom_16]}
-                                onChangeText={setToken}
-                                placeholder="Digite o token, caso nao tenha, preencha com QUALQUER valor"
-                                value={token}
-                            />
-
-                            <Text style={[commonStyle.common_margin_bottom_8, commonStyle.common_fontWeight_600, { color: '#fff', fontSize: 16 }]}>Modelo de Impressão Nome Cotação</Text>
-
-                            <TextInput
-                                style={[commonStyle.common_input, commonStyle.common_margin_bottom_16]}
-                                onChangeText={setNomeCotacao}
-                                placeholder="Digite o valor nome cotação"
-                                value={nomeCotacao}
-                            />
-
-                            <TouchableHighlight
-                                onPress={create}
-                                style={commonStyle.common_button_style}
-                                underlayColor='white'
-                            >
-                                <Text style={[commonStyle.common_text_button_style]}>Salvar</Text>
-                            </TouchableHighlight>
-
-                            <TouchableHighlight
-                                onPress={cancelEdit}
-                                style={commonStyle.common_button_style}
-                                underlayColor='white'
-                            >
-                                <Text style={[commonStyle.common_text_button_style]}>Cancelar</Text>
-                            </TouchableHighlight>
+                        <View style={[commonStyle.align_centralizar, commonStyle.common_rowItem]}>
+                            <Text style={[commonStyle.text_size_20, { color: "#FFF" }]}>{`Status: ${isEnviromentValidated ? 'Validado' : 'Nao validado'}`}</Text>
+                            {isEnviromentValidated ? <CustomIcon iconColor="#00FF00" icon={ICON_NAME.SUCCESS} /> : <CustomIcon iconColor="#F44336" icon={ICON_NAME.TRISTE} />}
                         </View>
+
+                        {isEnviromentValidated && (
+                            <TouchableHighlight
+                                onPress={() => init(tenant, urlBase, token)}
+                                style={commonStyle.common_button_style}
+                                underlayColor='white'
+                            >
+                                <Text style={commonStyle.common_text_button_style}>Ir para Login</Text>
+                            </TouchableHighlight>
+                        )}
+
                     </View>
-                )}
-            </ImageBackground>
-        </View>
+                )
+                }
+
+                {
+                    !hasValue && (
+                        // Se não houver valores, exibir campos de entrada para configuração
+                        <View style={[commonStyle.common_margin_top_64, commonStyle.common_margin_horizontal]}>
+                            <View style={[commonStyle.common_columnItem]}>
+
+                                <View style={[commonStyle.common_rowItem, commonStyle.justify_content_space_btw, commonStyle.common_margin_bottom_8]}>
+                                    <Text style={[commonStyle.common_fontWeight_600, { color: '#fff', fontSize: 16 }]}>Tenant</Text>
+                                    <CustomIcon icon={ICON_NAME.CAMERA} iconColor="#fff" iconSize={28} onPress={() => {
+
+                                        navigate('Camera', {
+                                            previousScreen: 'Config_Ambiente'
+                                        })
+
+                                    }} />
+                                </View>
+                                <TextInput
+                                    style={[commonStyle.common_input, commonStyle.common_margin_bottom_16]}
+                                    onChangeText={setTenant}
+                                    placeholder="Digite o tenant"
+                                    keyboardType='decimal-pad'
+                                    value={tenant}
+                                />
+
+                                <Text style={[commonStyle.common_margin_bottom_8, commonStyle.common_fontWeight_600, { color: '#fff', fontSize: 16 }]}>URL</Text>
+
+                                <TextInput
+                                    style={[commonStyle.common_input, commonStyle.common_margin_bottom_16]}
+                                    onChangeText={setUrlBase}
+                                    placeholder="Digite a URL"
+                                    value={urlBase}
+                                />
+
+
+                                <Text style={[commonStyle.common_margin_bottom_8, commonStyle.common_fontWeight_600, { color: '#fff', fontSize: 16 }]}>Token</Text>
+
+                                <TextInput
+                                    style={[commonStyle.common_input, commonStyle.common_margin_bottom_16]}
+                                    onChangeText={setToken}
+                                    placeholder="Digite o token, caso nao tenha, preencha com QUALQUER valor"
+                                    value={token}
+                                />
+
+                                <Text style={[commonStyle.common_margin_bottom_8, commonStyle.common_fontWeight_600, { color: '#fff', fontSize: 16 }]}>Modelo de Impressão Nome Cotação</Text>
+
+                                <TextInput
+                                    style={[commonStyle.common_input, commonStyle.common_margin_bottom_16]}
+                                    onChangeText={setNomeCotacao}
+                                    placeholder="Digite o valor nome cotação"
+                                    value={nomeCotacao}
+                                />
+
+                                <TouchableHighlight
+                                    onPress={create}
+                                    style={commonStyle.common_button_style}
+                                    underlayColor='white'
+                                >
+                                    <Text style={[commonStyle.common_text_button_style]}>Salvar</Text>
+                                </TouchableHighlight>
+
+                                <TouchableHighlight
+                                    onPress={cancelEdit}
+                                    style={commonStyle.common_button_style}
+                                    underlayColor='white'
+                                >
+                                    <Text style={[commonStyle.common_text_button_style]}>Cancelar</Text>
+                                </TouchableHighlight>
+                            </View>
+                        </View>
+                    )
+                }
+            </ImageBackground >
+        </View >
     );
 }
 
